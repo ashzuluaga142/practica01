93848dcc2a3f301c041374ef9ebd0f9d556fb419
ashzuluaga142 <79558235+ashzuluaga142@users.noreply.github.com>
2021-02-25 01:00:39 -0500

    Actualizacion

--- src/App.js
+++ src/App.js
@@ -1,25 +1,123 @@
-import logo from './logo.svg';
-import './App.css';
+import { isEmpty, size } from 'lodash'
+import React, { useState }from 'react' /* importar  el primer estado hut */
+import shortid from 'shortid'
 
 function App() {
+  const [task, setTask]= useState("") /* cuando el  usuairo ingrese algo  */
+  const [tasks, setTasks] = useState([])
+  const [editMode, setEditMode]= useState(false)
+  const [id, setId]= useState("")
+
+  const addTask =(e) => {
+    e.preventDefault() /*para evitar ue nos recargue la pagina por el sudmit  */
+    /*para validar  si un campo este vacido o no esta vacido varias  
+    formas  podemos utilizar una librerias lodash  desde la terminar  cargamos el loash. yarn add lodash*/
+    if(isEmpty(task)){
+      console.log("Task empty")
+      return
+    } 
+     const newTask = {
+       id: shortid.generate(),
+       name: task
+     }
+     setTasks([...tasks, newTask ])
+
+     setTask("") /* despues de decir si  la tarea esta correcta lo vamos   a dejar nulo */
+  }
+
+  const saveTask =(e) => {
+    e.preventDefault() 
+
+    if(isEmpty(task)){
+      console.log("Task empty")
+      return
+    } 
+     const editedTasks = tasks.map(item =>item.id=== id ? {id, name : task} : item)
+     setTasks(editedTasks)
+     setEditMode(false)
+     setTask("") 
+     setId("")
+  }
+
+  const deleteTask = (id) =>{
+         const filterdTasks = tasks.filter(task => task.id !==id)
+         setTasks(filterdTasks)
+       }
+    
+
+   const editTask = (theTask) =>
+   {
+     setTask(theTask.name)
+     setEditMode(true)
+    setId(theTask.id)
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className ="container mt-5">
+      <h1>TAREAS</h1>
+      <hr/>
+         <div className="row">
+            <div className="col-8">
+              <h4 className="text-center">Lista de Tareas</h4>
+             
+                  {  
+               size(tasks) == 0 ?(
+                  <h5 className= "text-center">AUN NO HAY TAREAS PROGRAMADAS.</h5>
+                  ): (
+                    <ul className="list-group">
+                       {
+                        tasks.map((task) => ( 
+                         <li className="list-group-item"key={task.id} >
+                         <span className="lead">{task.name}</span>
+                         <button className="btn btn-danger btn-sm float-right mx-2"
+                         onClick={() => deleteTask(task.id) }
+                         >
+                        Eliminar
+                       </button> 
+                       <button className="btn btn-warning btn-sm float-right"
+                        onClick={() => editTask(task) }
+                       >
+                        Editarr
+                       </button>
+                      </li>
+                       ))
+                      }
+                    </ul>
+                  )
+                  
+              }
+
+            </div>
+            <div className="col-4">
+            <h4 className= "tex-center">
+             { editMode ? "Modificar Tarea" : "Agregar Tarea"}
+            </h4>
+            
+            <form onSubmit={editMode ? saveTask : addTask }> 
+              <input
+                type="text"
+                className="form-control mb-2"
+                placeholder="Ingrese la Tarea"
+                /* el metodo onchange nos hace una funcion tipo flecha  */
+                onChange={(text)=> setTask(text.target.value)}/* cuando   ingrese  algo en  tarea  
+                vamos  asginarle a la variale task */
+                value={task}/* limpiar la tarea  */
+              />     
+            
+            <button
+             className={ editMode ? "btn btn-warning btn-block" : "btn btn-dark btn-block"}
+              /* como colocamos un  boton tipo submit "va a disparar la accion del  formulario y se modifica e
+              el metodo <form onSubmit={addTask}>"cuando el metodo requiere parametros  tenemos qye  hacer la funciona  flecha
+              pero en este caso  no necesitamos parametros */
+             type="submit"
+            >
+              { editMode ? "Guardar" : "Agregar"}
+            </button>
+            </form>
+            </div>
+         </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
